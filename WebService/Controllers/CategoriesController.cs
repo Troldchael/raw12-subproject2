2aa30ddd217785c6952e590d70455d4222b5a973
Henrik Bulskov <bulskov@ruc.dk>
2020-10-27 11:06:48 +0100

    Adding routing and action results

--- WebService/Controllers/CategoriesController.cs
+++ WebService/Controllers/CategoriesController.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataServiceLib;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebService.Controllers
 {
-    public class CategoriesController
+    [ApiController]
+    public class CategoriesController : ControllerBase
     {
-        
+
+
+        [HttpGet("api/categories")]
+        public IActionResult GetCategories()
+        {
+            var ds = new DataService();
+            var categories = ds.GetCategories();
+
+            return Ok(categories);
+        }
+
+        [HttpGet("api/categories/{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var ds = new DataService();
+            var category = ds.GetCategory(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
     }
 }
