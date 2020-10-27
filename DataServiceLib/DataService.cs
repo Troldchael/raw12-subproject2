da3b8c177fd38a65436741f34636a3a81d889954
Henrik Bulskov <bulskov@ruc.dk>
2020-10-27 10:46:29 +0100

    Add dataservice

---
+++ DataServiceLib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServiceLib
+{
+    public class DataService
+    {
+        private List<Category> _categories = new List<Category>
+        {
+            new Category{ Id = 1, Name = "Beverages", Description = "Soft drinks, coffees, teas, beers, and ales"},
+            new Category{ Id = 2, Name = "Condiments", Description = "Sweet and savory sauces, relishes, spreads, and seasonings"},
+            new Category{ Id = 3, Name = "Confections", Description = "Desserts, candies, and sweet breads"},
+            new Category{ Id = 4, Name = "Dairy Products", Description = "Cheeses"},
+            new Category{ Id = 5, Name = "Grains/Cereals", Description = "Breads, crackers, pasta, and cereal"},
+            new Category{ Id = 6, Name = "Meat/Poultry", Description = "Prepared meats"},
+            new Category{ Id = 7, Name = "Produce", Description = "Dried fruit and bean curd"},
+            new Category{ Id = 8, Name = "Seafood", Description = "Seaweed and fish"}
+        };
+
+
+        public IList<Category> GetCategories()
+        {
+            return _categories;
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _categories.FirstOrDefault(x => x.Id == id);
+        }
+
+    }
+}
