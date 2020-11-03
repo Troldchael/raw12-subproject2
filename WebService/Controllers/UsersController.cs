150dfb17bc4cccde2f6b0a61b0eba002b82aebe3
Nicooo <43990037+Troldchael@users.noreply.github.com>
2020-11-03 14:00:46 +0100

    getusers method work ajjajajaajaj

---
+++ WebService/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataServiceLib.Framework;
+using Microsoft.AspNetCore.Mvc;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        IDataService _dataService;
+        private readonly IMapper _mapper;
+
+        public UsersController(IDataService dataService, IMapper mapper)
+        {
+            _dataService = dataService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _dataService.GetUsers();
+            return Ok(_mapper.Map<IEnumerable<UsersDto>>(users));
+
+        }
+
+    }
+
+
+}
