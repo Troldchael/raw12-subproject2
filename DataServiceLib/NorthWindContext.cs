3f8c11df8ef64fc3a3ebb31de36d53332662111e
Nicooo <43990037+Troldchael@users.noreply.github.com>
2020-11-03 11:34:39 +0100

    getusers method started

--- DataServiceLib/NorthWindContext.cs
+++ DataServiceLib/NorthWindContext.cs
@@ -15,48 +15,21 @@ namespace DataServiceLib
         public static readonly ILoggerFactory MyLoggerFactory
             = LoggerFactory.Create(builder => { builder.AddConsole(); });
 
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Users> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLoggerFactory(MyLoggerFactory);
-            optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=postgres;pwd=KaffeKop+1");
+            optionsBuilder.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=Kiwikatte2");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Category>().ToTable("categories");
-            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
-            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
-            modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
-
-            modelBuilder.Entity<Product>().ToTable("products");
-            modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
-            modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
-            modelBuilder.Entity<Product>().Property(x => x.UntPrice).HasColumnName("unitprice");
-            modelBuilder.Entity<Product>().Property(x => x.QuantityPerUnit).HasColumnName("quantityperunit");
-            modelBuilder.Entity<Product>().Property(x => x.UnitsInStock).HasColumnName("unitsinstock");
-            modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
-
-            modelBuilder.Entity<Order>().ToTable("orders");
-            modelBuilder.Entity<Order>().Property(x => x.OrderId).HasColumnName("orderid");
-            modelBuilder.Entity<Order>().Property(x => x.Date).HasColumnName("orderdate");
-            modelBuilder.Entity<Order>().Property(x => x.Require).HasColumnName("requireddate");
-            modelBuilder.Entity<Order>().Property(x => x.Shipped).HasColumnName("shippeddate");
-            modelBuilder.Entity<Order>().Property(x => x.Freight).HasColumnName("freight");
-            modelBuilder.Entity<Order>().Property(x => x.ShipName).HasColumnName("shipname");
-            modelBuilder.Entity<Order>().Property(x => x.ShipCity).HasColumnName("shipcity");
-
-            modelBuilder.Entity<OrderDetails>().ToTable("orderdetails").HasKey(x => new {x.OrderId, x.ProductId});
-            modelBuilder.Entity<OrderDetails>().Property(x => x.UnitPrice).HasColumnName("unitprice");
-            modelBuilder.Entity<OrderDetails>().Property(x => x.Quantity).HasColumnName("quantity");
-            modelBuilder.Entity<OrderDetails>().Property(x => x.Discount).HasColumnName("discount");
-            modelBuilder.Entity<OrderDetails>().Property(x => x.OrderId).HasColumnName("orderid");
-            modelBuilder.Entity<OrderDetails>().Property(x => x.ProductId).HasColumnName("productid");
-            
+            modelBuilder.Entity<Users>().ToTable("users");
+            modelBuilder.Entity<Users>().Property(x => x.UserId).HasColumnName("user_id");
+            modelBuilder.Entity<Users>().Property(x => x.Username).HasColumnName("username");
+            modelBuilder.Entity<Users>().Property(x => x.Email).HasColumnName("email");
+            modelBuilder.Entity<Users>().Property(x => x.Password).HasColumnName("password");
         }
     }
 
---
+++ WebService/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataServiceLib.Framework;
+using Microsoft.AspNetCore.Mvc;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        IDataService _dataService;
+        private readonly IMapper _mapper;
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _dataService.GetUsers();
+            return Ok(_mapper.Map<IEnumerable<UsersDto>>(users));
+        }
+
+
+    }
+
+
+}
