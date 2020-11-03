4a1f9bd197eba6d9b8d19c61a5555d31f7e28fbe
Nicooo <43990037+Troldchael@users.noreply.github.com>
2020-11-03 09:37:01 +0100

    frameworks added

--- DataServiceLib/NorthWindContext.cs
+++ DataServiceLib/NorthWindContext.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataServiceLib.Framework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
