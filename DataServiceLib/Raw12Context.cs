a2764af073c5a5cc5b4fd0a8c6a8669e527d2e10
ChilliMads <55391223+ChilliMads@users.noreply.github.com>
2020-11-10 10:53:41 +0100

    Update Raw12Context.cs

--- DataServiceLib/Raw12Context.cs
+++ DataServiceLib/Raw12Context.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataServiceLib.Framework;
+using DataServiceLib.Moviedata;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -17,10 +18,17 @@ namespace DataServiceLib
 
         public DbSet<Users> Users { get; set; }
 
+        public DbSet<Details> Details { get; set; }
+
+        public DbSet<Actors> Actors { get; set; }
+
+        public DbSet<Genres> Genres { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLoggerFactory(MyLoggerFactory);
-            optionsBuilder.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=Kiwikatte2");
+            optionsBuilder.UseNpgsql("host=localhost;db=imdb.2;uid=postgres;pwd=1234");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -30,6 +38,22 @@ namespace DataServiceLib
             modelBuilder.Entity<Users>().Property(x => x.Username).HasColumnName("username");
             modelBuilder.Entity<Users>().Property(x => x.Email).HasColumnName("email");
             modelBuilder.Entity<Users>().Property(x => x.Password).HasColumnName("password");
+
+            modelBuilder.Entity<Details>().ToTable("title").HasKey(x => x.Tconst);
+            modelBuilder.Entity<Details>().Property(x => x.Tconst).HasColumnName("tconst");
+            modelBuilder.Entity<Details>().Property(x => x.TitleType).HasColumnName("titletype");
+            modelBuilder.Entity<Details>().Property(x => x.OriginalTitle).HasColumnName("originaltitle");
+
+            modelBuilder.Entity<Actors>().ToTable("name").HasKey(x => x.Nconst);
+            modelBuilder.Entity<Actors>().Property(x => x.Nconst).HasColumnName("nconst");
+            modelBuilder.Entity<Actors>().Property(x => x.PrimaryName).HasColumnName("primaryname");
+
+            modelBuilder.Entity<Genres>().ToTable("genres").HasKey(x => x.Tconst);
+            modelBuilder.Entity<Genres>().Property(x => x.Tconst).HasColumnName("tconst");
+            modelBuilder.Entity<Genres>().Property(x => x.Genre).HasColumnName("genre");
+
+
+
         }
     }
 
