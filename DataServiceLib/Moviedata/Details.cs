f11870f70dd9ab0c7788f45c256d01f296cc668d
ChilliMads <55391223+ChilliMads@users.noreply.github.com>
2020-11-10 10:53:36 +0100

    Update Details.cs

--- DataServiceLib/Moviedata/Details.cs
+++ DataServiceLib/Moviedata/Details.cs
@@ -2,10 +2,12 @@
 {
     public class Details
     {
+
+        public string Tconst { get; set; }
         //properties
-        public string Type1 { get; set; }
+        public string TitleType { get; set; }
 
         //navigation poperties
-        public string Type { get; set; } //dummy
+        public string OriginalTitle { get; set; } //dummy
     }
 }
