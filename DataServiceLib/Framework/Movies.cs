490d4927fb3e29d89ac43c36ee4525a5eb0c38d4
BlankGlitchy <55390034+BlankGlitchy@users.noreply.github.com>
2020-11-10 13:50:40 +0100

    Create Movies.cs

---
+++ DataServiceLib/Framework/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServiceLib.Framework
+{
+    public class Movies
+    {
+        //properties
+        public int titleID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        
+    }
+}
