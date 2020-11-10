6742144f870fc45a5591158d3c8420a37bdf1994
ChilliMads <55391223+ChilliMads@users.noreply.github.com>
2020-11-10 10:53:44 +0100

    Create ActorController.cs

---
+++ WebService/Controllers/ActorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataServiceLib.Framework;
+using DataServiceLib.Moviedata;
+using Microsoft.AspNetCore.Mvc;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("api/actors")]
+    public class ActorController : ControllerBase
+    {
+        IDataService _dataService;
+        private readonly IMapper _mapper;
+
+        public ActorController(IDataService dataService, IMapper mapper)
+        {
+            _dataService = dataService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetActors()
+        {
+            var name = _dataService.GetActors();
+            return Ok(_mapper.Map<IEnumerable<Actors>>(name));
+
+        }
+
+    }
+
+
+}
