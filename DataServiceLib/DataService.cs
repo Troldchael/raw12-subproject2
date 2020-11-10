444bd1f43d03824f50328398da283d5c63ba5a67
ChilliMads <55391223+ChilliMads@users.noreply.github.com>
2020-11-10 10:53:19 +0100

    Update DataService.cs

--- DataServiceLib/DataService.cs
+++ DataServiceLib/DataService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using DataServiceLib.Moviedata;
 
 namespace DataServiceLib.Framework
 {
@@ -11,10 +12,35 @@ namespace DataServiceLib.Framework
     {
         private List<Users> _users = new List<Users>();
 
-        public IList<Users> GetUsers()
+            public IList<Users> GetUsers()
+            {
+                var ctx = new Raw12Context();
+                return ctx.Users.ToList();
+            }
+
+        private List<Details> _details = new List<Details>();
+
+        public IList<Details> GetDetails()
         {
             var ctx = new Raw12Context();
-            return ctx.Users.ToList();
+            return ctx.Details.ToList();
+        }
+
+        private List<Actors> _actors = new List<Actors>();
+
+        public IList<Actors> GetActors()
+        {
+            var ctx = new Raw12Context();
+            return ctx.Actors.ToList();
+        }
+
+        private List<Genres> _genres = new List<Genres>();
+
+        public IList<Genres> GetGenres()
+        {
+            var ctx = new Raw12Context();
+            return ctx.Genres.ToList();
         }
     }
 }
+// Test test
