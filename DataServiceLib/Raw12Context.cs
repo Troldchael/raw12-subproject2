821f46e2efc52bea1a4accb083396a4728d2a4b1
Troldchael <nianmo@ruc.dk>
2020-11-11 16:12:12 +0100

    added all framework entities

--- DataServiceLib/Raw12Context.cs
+++ DataServiceLib/Raw12Context.cs
@@ -30,11 +30,38 @@ namespace DataServiceLib
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // framework entities
+            // users table 
             modelBuilder.Entity<Users>().ToTable("users").HasKey(x => x.UserId);
             modelBuilder.Entity<Users>().Property(x => x.UserId).HasColumnName("user_id");
             modelBuilder.Entity<Users>().Property(x => x.Username).HasColumnName("username");
             modelBuilder.Entity<Users>().Property(x => x.Email).HasColumnName("email");
             modelBuilder.Entity<Users>().Property(x => x.Password).HasColumnName("password");
+
+            // search_history table
+            modelBuilder.Entity<SearchHistory>().ToTable("search_history").HasKey(x => x.UserId);
+            modelBuilder.Entity<SearchHistory>().Property(x => x.UserId).HasColumnName("user_id");
+            modelBuilder.Entity<SearchHistory>().Property(x => x.Timestamp).HasColumnName("timestamp");
+            modelBuilder.Entity<SearchHistory>().Property(x => x.Keyword).HasColumnName("keyword");
+
+            // rating_history table
+            modelBuilder.Entity<RatingHistory>().ToTable("rating_history").HasKey(x => x.UserId);
+            modelBuilder.Entity<RatingHistory>().Property(x => x.UserId).HasColumnName("user_id");
+            modelBuilder.Entity<RatingHistory>().Property(x => x.Rating).HasColumnName("rating");
+            modelBuilder.Entity<RatingHistory>().Property(x => x.TitleId).HasColumnName("title_id");
+
+            // actor_bookmarking table
+            modelBuilder.Entity<ActorBookmarking>().ToTable("actor_bookmarking").HasKey(x => x.UserId);
+            modelBuilder.Entity<ActorBookmarking>().Property(x => x.UserId).HasColumnName("user_id");
+            modelBuilder.Entity<ActorBookmarking>().Property(x => x.Nconst).HasColumnName("nconst");
+
+            // title_bookmarking table
+            modelBuilder.Entity<TitleBookmarking>().ToTable("title_bookmarking").HasKey(x => x.UserId);
+            modelBuilder.Entity<TitleBookmarking>().Property(x => x.UserId).HasColumnName("user_id");
+            modelBuilder.Entity<TitleBookmarking>().Property(x => x.TitleId).HasColumnName("title_id");
+
+            // Movie data entities
+            //make diz
         }
     }
 
